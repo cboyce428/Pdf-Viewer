038b0ce9c20b554d325bc48f248c1b180ab3709c
cboyce428 <cboyce428@gmail.com>
2023-03-25 21:56:05 +0000

    Update FileUtils.kt

--- pdfViewer/src/main/java/com/rajat/pdfviewer/util/FileUtils.kt
+++ pdfViewer/src/main/java/com/rajat/pdfviewer/util/FileUtils.kt
@@ -1,9 +1,6 @@
 package com.rajat.pdfviewer.util
 
 import android.content.Context
-import android.os.Environment
-import android.provider.MediaStore
-import android.text.TextUtils
 import java.io.*
 
 object FileUtils {
@@ -22,7 +19,7 @@ object FileUtils {
         var outputStream: OutputStream? = null
         try {
                 outputStream = FileOutputStream(output)
-            var read = 0
+            var read: Int
             val bytes = ByteArray(1024)
             while (inputStream!!.read(bytes).also { read = it } != -1) {
                 outputStream.write(bytes, 0, read)
@@ -36,16 +33,4 @@ object FileUtils {
         }
     }
 
-    @Throws(IOException::class)
-    fun downloadFile(context: Context, assetName: String, filePath: String, fileName: String?){
-
-       val dirPath = "${Environment.getExternalStorageDirectory()}/${filePath}"
-        val outFile = File(dirPath)
-        //Create New File if not present
-        if (!outFile.exists()) {
-            outFile.mkdirs()
-        }
-        val outFile1 = File(dirPath, "/$fileName.pdf")
-        copy(context.assets.open(assetName), outFile1)
-    }
 }
